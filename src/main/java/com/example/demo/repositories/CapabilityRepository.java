9270ed3c3d79fd492cc314a9e06ddcad459f7b90
Faxiang Li <lifa0917@gmail.com>
2019-02-19 08:47:23 -0500

    Add repository

---
+++ src/main/java/com/example/demo/repositories/CapabilityRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import com.example.demo.domain.Capability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CapabilityRepository extends JpaRepository <Capability,Long>{
+
+
+}
