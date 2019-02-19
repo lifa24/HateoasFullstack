2e526de1264dcd03b6c7513dd2b8ebf66ac40bf2
Faxiang Li <lifa0917@gmail.com>
2019-02-19 09:05:35 -0500

    Add capability not found exception

---
+++ src/main/java/com/example/demo/exceptions/CapabilityNotFoundResponse.java
@@ -0,0 +1,9 @@
+package com.example.demo.exceptions;
+
+public class CapabilityNotFoundResponse {
+    private String capabilityNotFound;
+
+    public CapabilityNotFoundResponse(String capabilityNotFound) {
+        this.capabilityNotFound = capabilityNotFound;
+    }
+}
