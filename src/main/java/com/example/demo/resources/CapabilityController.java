3bf15bc1079ed2c22add408450c3db07b3e6b22d
Faxiang Li <lifa0917@gmail.com>
2019-02-20 22:07:50 -0500

    Add get all method

--- src/main/java/com/example/demo/resources/CapabilityController.java
+++ src/main/java/com/example/demo/resources/CapabilityController.java
@@ -3,10 +3,14 @@ package com.example.demo.resources;
 import com.example.demo.domain.Capability;
 import com.example.demo.services.CapabilityService;
 import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 @RestController
 @RequestMapping("/dashboard")
@@ -16,6 +20,19 @@ public class CapabilityController {
 
     public CapabilityController(CapabilityService capabilityService) {
         this.capabilityService = capabilityService;
+    }
+
+    @GetMapping
+    public Resources<Resource<Capability>> getAllCapabilities(){
+
+        List<Resource<Capability>> capabilities = capabilityService.getAllCapabilities().stream()
+                .map(capability -> new Resource<>(capability,
+                        linkTo(methodOn(CapabilityController.class).getCapability(capability.getId())).withRel("getThisCapability"),
+                        linkTo(methodOn(CapabilityController.class).getAllCapabilities()).withRel("getAllCapabilities")
+                )).collect(Collectors.toList());
+
+        return new Resources<>(capabilities);
+
     }
 
     @GetMapping("/{id}")
