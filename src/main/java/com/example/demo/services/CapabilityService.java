024cff40a0571f9216d5ae231152b63bc7c73c71
Faxiang Li <lifa0917@gmail.com>
2019-02-21 08:33:19 -0500

    Add error map

--- src/main/java/com/example/demo/services/CapabilityService.java
+++ src/main/java/com/example/demo/services/CapabilityService.java
@@ -3,8 +3,13 @@ package com.example.demo.services;
 import com.example.demo.domain.Capability;
 import com.example.demo.exceptions.CapabilityException;
 import com.example.demo.repositories.CapabilityRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -27,5 +32,15 @@ public class CapabilityService {
 
     public Capability saveCapability(Capability capability){
         return capabilityRepository.save(capability);
+    }
+
+    public ResponseEntity<?> errorMap(BindingResult result){
+        var errorM= new HashMap<>();
+
+        for(FieldError error: result.getFieldErrors()){
+            errorM.put(error.getField(),error.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(errorM, HttpStatus.BAD_REQUEST);
     }
 }
