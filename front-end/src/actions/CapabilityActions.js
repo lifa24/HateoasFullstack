a434d57f2be38951438f88863d20fad336e6436b
Faxiang Li <lifa0917@gmail.com>
2019-02-24 21:54:08 -0500

    Set up delete action

--- front-end/src/actions/CapabilityActions.js
+++ front-end/src/actions/CapabilityActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_CAPABILITIES } from "./ActionTypes";
+import { GET_CAPABILITIES, DELETE_CAPABILITY } from "./ActionTypes";
 
 export const getAllCapabilities = () => async dispatch => {
   const res = await axios.get("http://localhost:8080/dashboard");
@@ -7,5 +7,13 @@ export const getAllCapabilities = () => async dispatch => {
   dispatch({
     type: GET_CAPABILITIES,
     payload: res.data._embedded.capabilityList
+  });
+};
+
+export const deleteCapability = id => async dispatch => {
+  await axios.delete(`http://localhost:8080/dashboard/${id}`);
+  dispatch({
+    type: DELETE_CAPABILITY,
+    payload: id
   });
 };
